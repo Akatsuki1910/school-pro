c0ba9c99ecb388f71e090c8920ec6e7d1b3ea6dd
akatsuki1910 <akatsukicirno@gmail.com>
2020-10-28 11:20:57 +0900

    suchi3

---
+++ suchi3.java
@@ -0,0 +1,28 @@
+public class Main{
+	public static void main(String[] args) {
+		double arr[][] = {
+			{2,-1,0,1},
+			{-1,2,-1,0},
+			{0,-1,2,1}
+		};
+
+		double syoki[] = {1,1,1};
+
+		for(int i=0;i<30;i++){
+			for(int l=0;l<syoki.length;l++){
+				double p = 0;
+				for(int m=0;m<syoki.length;m++){
+					if(m==l){
+						continue;
+					}
+					p+=arr[l][m]*syoki[l];
+				}
+				syoki[l] = (arr[l][arr[l].length-1]-p)/arr[l][l];
+			}
+		}
+
+		for (double k: syoki){
+			System.out.println(String.format("%5.3f",k));
+		}
+	}
+}
